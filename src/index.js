e7cb496a45ed27fa9a463a7db789a05ec7114255
Nara <jmnarayani@gmail.com>
2025-07-15 11:19:25 -0400

    added cart context, shopping icon as well as cart dropdown

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { UserProvider } from './contexts/user.context';
 import { ProductsProvider } from './contexts/products.context';
+import { CartProvider } from './contexts/cart.context';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,7 +16,9 @@ ReactDOM.render(
     {/* Scope of storage for User provider */}
     <UserProvider> 
       <ProductsProvider>
+        <CartProvider>
         <App />
+        </CartProvider>
       </ProductsProvider>
     </UserProvider>
     </BrowserRouter>
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -7,16 +7,19 @@ import {ReactComponent as CrwnLogo} from '../../assets/crown.svg'
 import './navigation.styles.scss'
 import {useContext} from 'react'
 import {UserContext} from '../../contexts/user.context';
+import { CartContext } from "../../contexts/cart.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
-
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 const Navigation = () => {
   //we dont need a setter method for the user because the UseContextListener will handle this for us
   const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
 
   const signOutHandler = async ()=>{
     await signOutUser();
     // setCurrentUser(null);
-  };
+  }; 
 
   return(
   //Should be the / component since it is supposed to be displayed all the time 
@@ -34,7 +37,9 @@ const Navigation = () => {
             (<span className = 'nav-link' onClick={signOutHandler}>SIGN OUT</span>):
             (<Link className="nav-link" to="auth">Sign In</Link>)
         }
+    <CartIcon/>
     </div>
+    {isCartOpen && <CartDropdown/>}
   </div>
   <Outlet />
 </>
